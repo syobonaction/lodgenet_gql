42ddf71242aafcb831fab578f053a2732102bd00
Charles Cole <sismondi@Nicolette.local>
2023-08-22 21:04:25 -0500

    Completed the weatherLocations endpoint.

---
+++ graph/datasources/weather-api.go
@@ -0,0 +1,66 @@
+package datasources
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+var client http.Client
+
+type Request struct {
+	Type     string
+	Query    string
+	Language string
+	Unit     string
+}
+
+type Location struct {
+	Name        string
+	Country     string
+	Region      string
+	Lat         string
+	Lon         string
+	Timezone_id string
+}
+
+type Current struct {
+	Observation_time     string
+	Temperature          int
+	Weather_code         int
+	Weather_icons        []string
+	Weather_descriptions []string
+	Wind_speed           int
+	Wind_degree          int
+	Wind_dir             string
+	Pressure             int
+	Precip               int
+	Humidity             int
+	Cloudcover           int
+	Feelslike            int
+	Uv_index             int
+	Visibility           int
+	Is_day               string
+}
+
+type WeatherData struct {
+	Request  Request
+	Location Location
+	Current  Current
+}
+
+func GetWeather(target interface{}, zip string) error {
+	api_key := os.Getenv("API_KEY")
+	url := "http://api.weatherstack.com/current?access_key=" + api_key + "&query=" + zip + ""
+	resp, err := client.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	final := json.NewDecoder(resp.Body).Decode(target)
+
+	return final
+}
---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/subosito/gotenv"
+	"netreality.world/m/graph"
+)
+
+const defaultPort = "8080"
+
+func init() {
+	gotenv.Load()
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
